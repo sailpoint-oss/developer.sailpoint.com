8a70d9c6ffbd94a4d427359a5c6e02504584f686
Jordan Violet <jordan.violet@sailpoint.com>
2022-08-31 10:18:55 -0400

    updated favicon, removed unused docusaurus config variables

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,18 +6,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Developer Community',
-  tagline: 'Developers are cool',
+  title: 'SailPoint Developer Community',
   url: 'https://developer.sailpoint.com',
   baseUrl: '/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'SailPoint', // Usually your GitHub org/user name.
-  projectName: 'SailPoint Developer Community', // Usually your repo name.
-
+  // /static folder
+  favicon: 'img/SailPoint-Logo-Icon.ico',
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".

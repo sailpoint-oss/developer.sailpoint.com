1556c59b0e85215d50661a04d0f78a0458bd264e
philip-ellis-sp <philip.ellis@sailpoint.com>
2022-09-02 15:02:53 -0400

    added function for source files

--- src/components/HomepageBasics/index.js
+++ src/components/HomepageBasics/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./styles.module.css";
 import Link from '@docusaurus/Link';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 export default function HomepageBasics({link, title, image, description, buttonText}) {
 
   let linkHtml = <div></div>
@@ -14,7 +15,7 @@ export default function HomepageBasics({link, title, image, description, buttonT
   }
   return (
         <div className={styles.gettingStartedText}>
-          <img className={styles.gettingStartedCardIcon} src={image}></img>
+          <img className={styles.gettingStartedCardIcon} src={useBaseUrl(image)}></img>
           <div className={styles.gettingStartedOne}>{title}</div>
           <div className={styles.gettingStartedThree} dangerouslySetInnerHTML={{ __html: description }} ></div>
           {linkHtml}
--- src/components/TeamCard/index.js
+++ src/components/TeamCard/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import styles from "./styles.module.css";
 import Link from '@docusaurus/Link';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 export default function TeamCard({link, title, name, expertise, image}) {
 
   return (
     <Link to={link}>
       <div className={styles.card}>
       <div className={styles.cardFaceContainer}>
-        <img className={styles.cardFace} src={image}></img>
+        <img className={styles.cardFace} src={useBaseUrl(image)}></img>
       </div>
-      <img className={styles.cardArrow} src="/homepage/arrow-right.png"></img>
+      <img className={styles.cardArrow} src={useBaseUrl("/homepage/arrow-right.png")}></img>
       <div className={styles.cardText}>{name}</div>
       <div className={styles.titleText}>{title}</div>
       <div className={styles.expertiseText}>{expertise}</div>

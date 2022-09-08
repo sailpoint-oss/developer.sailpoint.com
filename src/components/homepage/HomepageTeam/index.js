d8238e67164db09a38d4fdd2d6ab8ade9aa9f943
jordan-violet-sp <jordan.violet@sailpoint.com>
2022-09-08 11:32:19 -0400

    fixing links on homepage

--- src/components/homepage/HomepageTeam/index.js
+++ src/components/homepage/HomepageTeam/index.js
@@ -7,10 +7,10 @@ export default function HomepageTeam() {
   return (
     <div className={styles.center}>
       <div className={styles.gridContainer}>
-        <TeamCard link={"https://developer.sailpoint.com/discuss/new-message?username=colin_mckibben"} name={"Colin McKibben"} title={"DEVELOPER ADVOCATE"} expertise={"API, WORKFLOWS, EVENT TRIGGERS"} image={"/homepage/colin-mckibben.png"}></TeamCard>
-        <TeamCard link={"https://developer.sailpoint.com/discuss/new-message?username=tyler_mairose"} name={"Tyler Mairose"} title={"DEVELOPER ADVOCATE"} expertise={"RULES, TRANSFORMS"} image={"/homepage/tyler-mairose.png"}></TeamCard>
-        <TeamCard link={"https://developer.sailpoint.com/discuss/new-message?username=philip-ellis"} name={"Philip Ellis"} title={"DEVELOPER ADVOCATE"} expertise={"TOOLS, SAAS CONNECTIVITY"} image={"/homepage/phil-ellis.png"}></TeamCard>
-        <TeamCard link={"https://developer.sailpoint.com/discuss/new-message?username=colin-mckibben"} name={"James Haytko"} title={"TECHNICAL WRITER"} expertise={"DEVELOPER DOCUMENTATION"} image={"/homepage/james-haytko.png"}></TeamCard>
+        <TeamCard link={"https://developer.sailpoint.com/discuss/u/colin_mckibben/summary"} name={"Colin McKibben"} title={"DEVELOPER ADVOCATE"} expertise={"API, WORKFLOWS, EVENT TRIGGERS"} image={"/homepage/colin-mckibben.png"}></TeamCard>
+        <TeamCard link={"https://developer.sailpoint.com/discuss/u/tyler_mairose/summary"} name={"Tyler Mairose"} title={"DEVELOPER ADVOCATE"} expertise={"RULES, TRANSFORMS"} image={"/homepage/tyler-mairose.png"}></TeamCard>
+        <TeamCard link={"https://developer.sailpoint.com/discuss/u/philip-ellis/summary"} name={"Philip Ellis"} title={"DEVELOPER ADVOCATE"} expertise={"DEVELOPER TOOLS, SAAS CONNECTIVITY"} image={"/homepage/phil-ellis.png"}></TeamCard>
+        <TeamCard link={"https://developer.sailpoint.com/discuss/u/jthaytko/summary"} name={"James Haytko"} title={"TECHNICAL WRITER"} expertise={"DEVELOPER DOCUMENTATION"} image={"/homepage/james-haytko.png"}></TeamCard>
       </div> 
     </div>
   );

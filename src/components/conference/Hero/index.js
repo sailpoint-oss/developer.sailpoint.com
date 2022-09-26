873016b46a680fc575f62fd18e3a8da60bbc514b
philip-ellis-sp <philip.ellis@sailpoint.com>
2022-09-26 17:15:20 -0400

    added mobile friendly signup screen

--- src/components/conference/Hero/index.js
+++ src/components/conference/Hero/index.js
@@ -4,6 +4,9 @@ import styles from "./styles.module.css";
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import ThemedImage from '@theme/ThemedImage';
 import { addDarkToFileName } from "../../../util/util";
+import { Widget } from '@typeform/embed-react'
+import { SliderButton } from '@typeform/embed-react'
+import { PopupButton } from '@typeform/embed-react'
 export default function ConferenceHero() {
   const background = '/conf/Shipyard-WaveBackground.png'
   const learnBuildDeploy = '/conf/LearnBuildDeploy-Light.png'
@@ -16,8 +19,11 @@ export default function ConferenceHero() {
 
   return (
     <div className={styles.mainCard}>
+      <div>
       <ThemedImage className={styles.background} sources={{light: useBaseUrl(background), dark: useBaseUrl(background)}}></ThemedImage>
       <ThemedImage className={styles.learnDeployText} sources={{light: useBaseUrl(learnBuildDeploy), dark: useBaseUrl(learnBuildDeploy)}}></ThemedImage>
+      </div>
+
       <div className={styles.signUpSection}>
         <div className={styles.imageContainer}>
           <ThemedImage className={styles.sailpointImage} sources={{light: useBaseUrl(sailpointImage), dark: useBaseUrl(sailpointImage)}}></ThemedImage>
@@ -31,15 +37,13 @@ export default function ConferenceHero() {
         <div className={styles.welcomeText}>
           Be the first to hear more about our innaugural conference for developers!
         </div>
-        <div className={styles.emailText}>
+        {/* <div className={styles.emailText}>
           Email Address
         </div>
-        <input className={styles.emailInput} type="text" id="fname" name="fname" placeholder="user.name@email.com"></input>
-        <div className={styles.button} onClick={handleSubmit}>
-          <div className={styles.link}>
+        <input className={styles.emailInput} type="text" id="fname" name="fname" placeholder="user.name@email.com"></input> */}
+        <SliderButton id="gVCODdMc" className={styles.button}>
             Join the Waitlist
-          </div>
-        </div>
+        </SliderButton >
       </div>
     </div>
   );

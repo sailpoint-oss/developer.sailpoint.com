18d6dc6b85b9d8ef010fb2c743644ae9c72552b9
philip-ellis-sp <philip.ellis@sailpoint.com>
2022-09-26 15:27:08 -0400

    added initial conference page

--- src/components/conference/Hero/index.js
+++ src/components/conference/Hero/index.js
@@ -1,10 +1,46 @@
 import React from "react";
 import clsx from "clsx";
 import styles from "./styles.module.css";
-import Link from '@docusaurus/Link';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import ThemedImage from '@theme/ThemedImage';
+import { addDarkToFileName } from "../../../util/util";
 export default function ConferenceHero() {
+  const background = '/conf/Shipyard-WaveBackground.png'
+  const learnBuildDeploy = '/conf/LearnBuildDeploy-Light.png'
+  const shipyardImage = '/conf/SailPoint-Shipyard-DeveloperConference-Logo.png'
+  const sailpointImage = '/img/SailPoint-Logo-RGB-Color.png'
+
+  function handleSubmit(e) {
+    console.log(e)
+  }
+
   return (
-    <div>
+    <div className={styles.mainCard}>
+      <ThemedImage className={styles.background} sources={{light: useBaseUrl(background), dark: useBaseUrl(background)}}></ThemedImage>
+      <ThemedImage className={styles.learnDeployText} sources={{light: useBaseUrl(learnBuildDeploy), dark: useBaseUrl(learnBuildDeploy)}}></ThemedImage>
+      <div className={styles.signUpSection}>
+        <div className={styles.imageContainer}>
+          <ThemedImage className={styles.sailpointImage} sources={{light: useBaseUrl(sailpointImage), dark: useBaseUrl(sailpointImage)}}></ThemedImage>
+        </div>
+        <div className={styles.imageContainer}>
+        <ThemedImage className={styles.shipyardLogo} sources={{light: useBaseUrl(shipyardImage), dark: useBaseUrl(shipyardImage)}}></ThemedImage>
+        </div>
+        <div className={styles.welcomeContainer}>
+          Welcome
+        </div >
+        <div className={styles.welcomeText}>
+          Be the first to hear more about our innaugural conference for developers!
+        </div>
+        <div className={styles.emailText}>
+          Email Address
+        </div>
+        <input className={styles.emailInput} type="text" id="fname" name="fname" placeholder="user.name@email.com"></input>
+        <div className={styles.button} onClick={handleSubmit}>
+          <div className={styles.link}>
+            Join the Waitlist
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

0c56bad34cfa15f02d6bba9b80f953ff3fdbfabb
Jordan Violet <69053702+jordan-violet-sp@users.noreply.github.com>
2022-09-28 20:57:42 -0400

    fixed image reference on page not found page

--- src/theme/NotFound.js
+++ src/theme/NotFound.js
@@ -1,27 +1,38 @@
-import React from 'react';
-import Translate, {translate} from '@docusaurus/Translate';
-import {PageMetadata} from '@docusaurus/theme-common';
-import Layout from '@theme/Layout';
+import React from "react";
+import Translate, { translate } from "@docusaurus/Translate";
+import { PageMetadata } from "@docusaurus/theme-common";
+import Layout from "@theme/Layout";
 export default function NotFound() {
   return (
     <>
       <PageMetadata
         title={translate({
-          id: 'theme.NotFound.title',
-          message: 'Page Not Found',
+          id: "theme.NotFound.title",
+          message: "Page Not Found",
         })}
       />
       <Layout>
-        <main style={{textAlign:"center"}} className="container margin-vert--xl">
+        <main
+          style={{ textAlign: "center" }}
+          className="container margin-vert--xl"
+        >
           <div className="row">
             <div className="col col--6 col--offset-3">
-              <h1 className="hero__title">
-                Page Not Found
-              </h1>
+              <h1 className="hero__title">Page Not Found</h1>
               <p>
-              Our docs and API specifications have recently moved—checkout the navbar at the top of this page to find their new home. If you can't find the document you're looking for, <a href="https://developer.sailpoint.com/discuss/docs-request" target="_blank" rel="noopener noreferrer">let us know</a>.
+                Our docs and API specifications have recently moved—checkout the
+                navbar at the top of this page to find their new home. If you
+                can't find the document you're looking for,{" "}
+                <a
+                  href="https://developer.sailpoint.com/discuss/docs-request"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  let us know
+                </a>
+                .
               </p>
-                <img height="60%" src="../img/not-found/codey.png"/>
+              <img height="60%" src="img/not-found/codey.png" />
             </div>
           </div>
         </main>

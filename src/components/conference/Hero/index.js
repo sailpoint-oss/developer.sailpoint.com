99e0031f6061ae2571b5cefe2f40b9c251e1ed7b
Jordan Violet <69053702+jordan-violet-sp@users.noreply.github.com>
2022-09-30 22:14:04 -0400

    changed coming soon text

--- src/components/conference/Hero/index.js
+++ src/components/conference/Hero/index.js
@@ -1,41 +1,33 @@
 import React from "react";
 import styles from "./styles.module.css";
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import ThemedImage from '@theme/ThemedImage';
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import ThemedImage from "@theme/ThemedImage";
 import { addDarkToFileName } from "../../../util/util";
-import { SliderButton } from '@typeform/embed-react'
+import { SliderButton } from "@typeform/embed-react";
 export default function ConferenceHero() {
-  const communication = '/conf/codey-surfing-small.png'
+  const communication = "/conf/codey-surfing-small.png";
 
   return (
-  <div className={styles.center}>
-   <div className={styles.mainCard}>
-      <div>
-        <div className={styles.headerSection}>
-          <div className={styles.shipyardText}>
-            Shipyard
-          </div>
-          <div className={styles.yearTextBorder}>
-            <div className={styles.yearText}>
-              2023
+    <div className={styles.center}>
+      <div className={styles.mainCard}>
+        <div>
+          <div className={styles.headerSection}>
+            <div className={styles.shipyardText}>Shipyard</div>
+            <div className={styles.yearTextBorder}>
+              <div className={styles.yearText}>2023</div>
             </div>
           </div>
-        </div>
 
-        <div className={styles.comingSoonText}>
-          COMING SOON | First Half of 2023
-        </div>
-        <div className={styles.descriptionText}>
-          The conference for developers on SailPoint platforms.
-        </div>
+          <div className={styles.comingSoonText}>COMING SOON | Q1 2023</div>
+          <div className={styles.descriptionText}>
+            The conference for developers on SailPoint platforms.
+          </div>
 
           <SliderButton id="gVCODdMc" className={styles.button}>
-              Join the Waitlist
-          </SliderButton >
-
+            Join the Waitlist
+          </SliderButton>
+        </div>
       </div>
     </div>
-    </div>
- 
   );
 }

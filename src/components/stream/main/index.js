02f1db6d8a0b549a564c897793407b66e42c2734
philip-ellis-sp <philip.ellis@sailpoint.com>
2022-11-28 17:04:53 -0500

    started adding styles

--- src/components/stream/main/index.js
+++ src/components/stream/main/index.js
@@ -7,26 +7,43 @@ export default function Main() {
     <div>
       <div className={styles.headerContainer}>
         <div className={styles.headerContent}>
-          <div>Lets talk platform</div>
-          <div>3:15 - 4:15 PM</div>
-          <div>Jane Doe, Engineering Manager</div>
+          <div className={styles.headerText}>Let's talk platform</div>
+          <div className={styles.timeText}>3:15 - 4:15 PM</div>
+          <div className={styles.speakerText}>JANE DOE, ENGINEERING MANAGER</div>
         </div>
 
         <div className={styles.headerContent}>
-          <span>Agenda</span>
-          <span>Speakers</span>
-          <span>FAQ</span>
+          <div className={styles.buttonsContainer}>
+            <div className={styles.buttonsContent}>
+              <div className={styles.button}>Agenda</div>
+            </div>
+            <div className={styles.buttonsContent}>
+              <div className={styles.button}>Speakers</div>
+            </div>
+            <div className={styles.buttonsContent}>
+              <div className={styles.button}>FAQ</div>
+            </div>
+          </div>
         </div>
       </div>
-      <div className={styles.stageButtonsContainer}>
-        <div className={styles.stageButtonsContent}>Main Stage</div>
-        <div className={styles.stageButtonsContent}>IdentityNow</div>
-        <div className={styles.stageButtonsContent}>IdentityIQ</div>
+      <div className={styles.center}>
+        <div className={styles.stageButtonsContainer}>
+          <div className={styles.stageButtonsContent}>
+              <div className={styles.stageButton}>Main Stage</div>
+          </div>
+          <div className={styles.stageButtonsContent}>
+            <div className={styles.stageButton}>IdentityNow</div>
+          </div>
+          <div className={styles.stageButtonsContent}>
+            <div className={styles.stageButton}>IdentityIQ</div>
+          </div>
+        </div>
+
       </div>
       <div className={styles.stageContainer}>
-        <div className={styles.stageContent}>Video Player</div>
-        <div className={styles.stageContent}>Chat Window</div>
-      </div>
+          <div className={styles.stageContent}>Video Player</div>
+          <div className={styles.stageContent}>Chat Window</div>
+        </div>
     </div>
   );
 }

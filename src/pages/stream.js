fc2c276813d425763f5e9c8799d1c997a5044a8d
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2023-01-11 16:01:40 -0600

    Update stream.js

--- src/pages/stream.js
+++ src/pages/stream.js
@@ -2,17 +2,14 @@ import React from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import Main from '../components/stream/main';
-import BrowserOnly from '@docusaurus/BrowserOnly';
 
 export default function Stream() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout>
-      <BrowserOnly>
-        <main>
-          <Main></Main>
-        </main>
-      </BrowserOnly>
+      <main>
+        <Main></Main>
+      </main>
     </Layout>
   );
 }

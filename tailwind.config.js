e0f68851ede00f1dfb1cb2f21271e157447a8b4c
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2023-01-11 14:15:44 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 // tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  corePlugins: {
+    preflight: true,
+  },
   content: ['./src/pages/*.js', './src/components/stream/**/*.js'],
   theme: {
     extend: {},

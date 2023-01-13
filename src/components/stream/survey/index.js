b39b271ab3ddac2bd52eeb26b3094a696e16f248
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2023-01-13 10:50:10 -0600

    Typeform swap

--- src/components/stream/survey/index.js
+++ src/components/stream/survey/index.js
@@ -1,56 +1,33 @@
-import React, { useEffect } from "react";
-import { Widget } from "@typeform/embed-react";
-import styles from "./styles.module.css";
-import Modal from "react-modal";
-export default function Survey({ id, stage, socket }) {
-  const [surveyModalIsOpen, setSurveyIsOpen] = React.useState(false);
+import React, {useEffect} from 'react';
+import {PopupButton} from '@typeform/embed-react';
+import styles from './styles.module.css';
+import Modal from 'react-modal';
+import {useRef} from 'react';
+
+export default function Survey({id, stage, socket}) {
+  const typeformRef = useRef(null);
+
   useEffect(() => {
     if (socket) {
-      socket.on("survey", (data) => {
-        console.log(data);
-        console.log(stage.stage);
+      socket.on('survey', (data) => {
         if (stage.stage === data) {
-          setSurveyIsOpen(true);
+          typeformRef.current.children[1].click();
         }
       });
       return () => {
-        socket.off("survey");
+        socket.off('survey');
       };
     }
-
   }, [stage]);
 
-  function openSurveyModal() {
-    setSurveyIsOpen(true);
-  }
-
-  function closeSurveyModal() {
-    setSurveyIsOpen(false);
-  }
-
   return (
-    <div>
-      <button
-        className="cursor-pointer border-[color:var(--ifm-color-primary)] md:grow border-2 hover:bg-[color:var(--ifm-color-primary)] hover:text-white text-[color:var(--ifm-color-primary)] text-center font-bold py-2 px-4 rounded"
-        onClick={openSurveyModal}
-      >
+    <div ref={typeformRef}>
+      <PopupButton
+        width={30}
+        id={id}
+        className="cursor-pointer border-[color:var(--ifm-color-primary)] md:grow border-2 hover:bg-[color:var(--ifm-color-primary)] hover:text-white text-[color:var(--ifm-color-primary)] text-center font-bold py-2 px-4 rounded">
         Survey
-      </button>
-      <Modal
-        isOpen={surveyModalIsOpen}
-        onRequestClose={closeSurveyModal}
-        className={styles.modal}
-        contentLabel="Survey"
-      >
-        <div className="md:w-[50vw] md:h-[50vh] w-[95vw] h-[80vh] overflow-hidden">
-          <Widget className="h-full w-full p-8" id={id}>
-            Join the Waitlist
-          </Widget>
-          <button className={styles.modalButton} onClick={closeSurveyModal}>
-            Close
-          </button>
-        </div>
-      </Modal>
+      </PopupButton>
     </div>
   );
 }

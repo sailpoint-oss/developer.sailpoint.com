407ae8b022af154c1395c3669b2b92629bcb21bd
tyler-mairose-sp <tyler.mairose@sailpoint.com>
2023-05-12 12:49:51 -0400

    Update Sidebar description for NERM

--- products/nerm/sidebar.js
+++ products/nerm/sidebar.js
@@ -7,7 +7,7 @@ const sidebars = {
           type: 'generated-index',
           title: 'NERM API',
           description:
-            "These are the NERM APIs for SailPoint. We encourage you to join the SailPoint Developer Community forum at https://developer.sailpoint.com/discuss to connect with other developers using our APIs.",
+            "These are the Non-employee Lifecycle Management APIs for SailPoint. We encourage you to join the SailPoint Developer Community forum at https://developer.sailpoint.com/discuss to connect with other developers using our APIs.",
           slug: '/api',
         },
         // @ts-ignore

834bef548f5bae69252e127c1b87c8368f4be834
tyler-mairose-sp <tyler.mairose@sailpoint.com>
2023-05-23 11:05:29 -0400

    Update navbar

--- navbar.js
+++ navbar.js
@@ -58,7 +58,13 @@ module.exports = {
       position: 'left',
       items: [
         {to: '#', label: 'API Specifications', className: 'navbar__section'},
-        {to: '/nerm/api', label: 'NERM APIs', className: 'indent'}
+        {to: '/nerm/api', label: 'NERM APIs', className: 'indent'},
+        {to: '#', label: 'External Links', className: 'navbar__section'},
+        {
+          href: 'https://documentation.sailpoint.com',
+          label: 'Product Documentation',
+          className: 'indent',
+        }
       ]
     },
     {

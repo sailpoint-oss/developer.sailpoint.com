328a2f9344b2397808305e042737ad17b00a60bf
Luke Hagar <lukeslakemail@gmail.com>
2023-07-25 07:55:07 -0500

    Functional AI Page

---
+++ src/pages/ai.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import Header from '../components/ServicesAI/Header';
+import Content from '../components/ServicesAI/Content';
+
+export default function AI() {
+  const {siteConfig} = useDocusaurusContext();
+
+  return (
+    <Layout description="The SailPoint Developer Community has everything you need to build, extend, and automate scalable identity solutions.">
+      <main>
+        <Header />
+        <Content />
+      </main>
+    </Layout>
+  );
+}

d4c1f42bffcccfa03f6cc24bb71406143f6eb492
james.haytko <james.haytko@sailpoint.com>
2023-08-03 12:22:59 -0500

    Added patch guide to sidebar.

--- products/idn/sidebar.js
+++ products/idn/sidebar.js
@@ -34,6 +34,10 @@ const sidebars = {
           id: 'api/postman-collections',
         },
         {
+          type: 'doc',
+          id: 'api/patch-guide',
+        }
+        {
           type: 'category',
           label: 'V3 APIs',
           link: {

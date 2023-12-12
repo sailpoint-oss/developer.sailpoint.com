5da55d575adca7a9f12caf0efe7d20c408224cae
Philip Ellis <philip.ellis@sailpoint.com>
2023-12-12 12:19:15 -0600

    added 404 response to lambda

--- backend/src/handlers/get-by-id.mjs
+++ backend/src/handlers/get-by-id.mjs
@@ -51,7 +51,17 @@ export const getByIdHandler = async (event) => {
     console.error("Error name:", err.name);
     console.error("Error stack:", err.stack);
   
-  throw err;
+    const response = {
+      statusCode: 404,
+      headers: {
+        "Access-Control-Allow-Headers" : "Content-Type",
+        "Access-Control-Allow-Origin": "*", //DO NOT USE THIS VALUE IN PRODUCTION - https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-cors.html
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+    },
+      body: JSON.stringify(item)
+      
+    };
+    return response;
   }
  
   const response = {

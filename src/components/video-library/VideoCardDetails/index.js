d56ce0b1a734d3c7b0058d26eedecd4964707cc2
darrell-thobe-sp <darrell.thobe@sailpoint.com>
2024-01-11 21:32:22 -0500

    styles for discourse comments

--- src/components/video-library/VideoCardDetails/index.js
+++ src/components/video-library/VideoCardDetails/index.js
@@ -20,7 +20,7 @@ const VideoCardDetail = (props) => {
           <div>
             <p>{props.route.customProps.body}</p>
           </div>
-          <div className={styles.discourseContainer}>
+          <div id='discourseContainer' className={styles.discourseContainer}>
             <DiscourseEmbed></DiscourseEmbed>
           </div>
         </div>

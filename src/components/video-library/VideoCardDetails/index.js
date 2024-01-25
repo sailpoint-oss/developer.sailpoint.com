86303c715d516939bfad5cd84e60311821e9bf11
darrell-thobe-sp <darrell.thobe@sailpoint.com>
2024-01-25 09:41:25 -0500

    testing style changes on discourse embed

--- src/components/video-library/VideoCardDetails/index.js
+++ src/components/video-library/VideoCardDetails/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Video from '../../Video.js';
 import styles from './styles.module.css';
 import Layout from '@theme/Layout';
@@ -6,6 +6,26 @@ import DiscourseEmbed from '../VideoComments/index.js';
 
 const VideoCardDetail = (props) => {
   const base = 'https://play.vidyard.com/';
+
+  useEffect(() => {
+    // This code would be part of your iframe's JavaScript
+    window.addEventListener('message', (event) => {
+      if (event.origin !== 'https://developer.identitysoon.com') {
+        // Replace with the actual domain of the parent
+        console.log('Message not from parent! Ignoring.');
+        return; // Only accept messages from the specified origin
+      }
+
+      console.log('Received message from parent: ', event.data);
+      const data = event.data;
+
+      if (data.action === 'changeStyle') {
+        Object.keys(data.styles).forEach((key) => {
+          document.body.style[key] = data.styles[key];
+        });
+      }
+    });
+  }, []);
 
   return (
     <Layout description="The SailPoint Developer Community has everything you need to build, extend, and automate scalable identity solutions.">
@@ -20,7 +40,7 @@ const VideoCardDetail = (props) => {
           <div>
             <p>{props.route.customProps.body}</p>
           </div>
-          <div id='discourseContainer' className={styles.discourseContainer}>
+          <div id="discourseContainer" className={styles.discourseContainer}>
             <DiscourseEmbed></DiscourseEmbed>
           </div>
         </div>

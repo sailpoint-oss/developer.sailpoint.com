f476a2c83f5f45b88af416329816637f3afda3b9
Tyler Mairose <tyler.mairose@sailpoint.com>
2024-05-08 16:15:56 -0400

    Revert webpack loader changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,16 +46,6 @@ const config = {
       }),
     ],
   ],
-  webpack: {
-    jsLoader: (isServer) => ({
-      loader: require.resolve("esbuild-loader"),
-      options: {
-        loader: "tsx",
-        format: isServer ? "cjs" : undefined,
-        target: isServer ? "node12" : "es2017",
-      },
-    }),
-  },
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({

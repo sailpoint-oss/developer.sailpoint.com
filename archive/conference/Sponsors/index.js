bd7adac7c5783dea922c377041296755cbf5d46c
darrell-thobe-sp <darrell.thobe@sailpoint.com>
2024-07-29 15:49:31 -0400

    removing archive folder from old streaming platform for devdays

--- archive/conference/Sponsors/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
-import Link from '@docusaurus/Link';
-export default function ConferenceSponsors() {
-  return <div></div>;
-}

ab2daea9171084e47844dba476c23c6d898f3410
darrell-thobe-sp <darrell.thobe@sailpoint.com>
2024-11-18 15:59:46 -0500

    adding dotenv

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,5 +1,6 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
+require('dotenv').config();
 
 const { themes } = require('prism-react-renderer');
 const lightCodeTheme = themes.github;

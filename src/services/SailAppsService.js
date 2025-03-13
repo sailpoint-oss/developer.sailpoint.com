c0a83d184e754821386e3fc804e59f60a3c8df48
Luke Hagar <lukeslakemail@gmail.com>
2025-03-12 22:53:13 -0500

    working on getting the endpoint accessible externally

--- src/services/SailAppsService.js
+++ src/services/SailAppsService.js
@@ -1,7 +1,7 @@
-export async function sendCode() {
+export async function sendCode(gatewayUrl, code) {
   try {
-    // const response = await fetch('https://api.sailapps.io/authenticate');
-    // return await response.json();
+    const response = await fetch(`${gatewayUrl}code/${code}`);
+    return await response.json();
   } catch (error) {
     return [];
   }

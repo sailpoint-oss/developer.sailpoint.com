974e0bdf5b50161138d88c2446e89f38769c716f
Luke Hagar <lukeslakemail@gmail.com>
2025-03-13 11:20:24 -0500

    adjusting exchange flow

--- backend/src/mithrandir/index.ts
+++ backend/src/mithrandir/index.ts
@@ -1,19 +1,25 @@
 // Create a DocumentClient that represents the query to add an item
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient, GetCommand, PutCommand, UpdateCommand } from '@aws-sdk/lib-dynamodb';
-import crypto, { createCipheriv } from 'crypto';
-import { Hono } from 'hono'
-import { handle } from 'hono/aws-lambda'
-import { HTTPException } from 'hono/http-exception'
+import {DynamoDBClient} from '@aws-sdk/client-dynamodb';
+import {
+  DynamoDBDocumentClient,
+  GetCommand,
+  PutCommand,
+  UpdateCommand,
+} from '@aws-sdk/lib-dynamodb';
+import crypto, {createCipheriv} from 'crypto';
+import {Hono} from 'hono';
+import {handle} from 'hono/aws-lambda';
+import {HTTPException} from 'hono/http-exception';
 
-const clientId = "sailpoint-cli"
-const redirectUrl = "https://developer.sailpoint.com/sailapps"
-const devRedirectUrl = "http://localhost:4200/sailapps"
+const clientId = 'sailpoint-cli';
+const redirectUrl = 'https://developer.sailpoint.com/sailapps';
+const devRedirectUrl = 'http://localhost:4200/sailapps';
 
-const app = new Hono()
+const app = new Hono();
 
 function generateRandomString(length: number) {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let result = '';
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters.length));
@@ -26,10 +32,12 @@ const ENDPOINT_OVERRIDE = process.env.ENDPOINT_OVERRIDE;
 let ddbClient = undefined;
 
 if (ENDPOINT_OVERRIDE) {
-  ddbClient = new DynamoDBClient({ endpoint: ENDPOINT_OVERRIDE });
+  ddbClient = new DynamoDBClient({endpoint: ENDPOINT_OVERRIDE});
 } else {
   ddbClient = new DynamoDBClient({}); // Use default values for DynamoDB endpoint
-  console.warn("No value for ENDPOINT_OVERRIDE provided for DynamoDB, using default");
+  console.warn(
+    'No value for ENDPOINT_OVERRIDE provided for DynamoDB, using default',
+  );
 }
 
 const ddbDocClient = DynamoDBDocumentClient.from(ddbClient);
@@ -42,222 +50,250 @@ const tableName = process.env.SAMPLE_TABLE;
 // This state value can be overridden by the client if they wish to provide their own encryption key alongside the pre-populated UUID
 app.post('/Prod/sailapps/uuid', async (c) => {
   if (c.req.header('Content-Type') !== 'application/json') {
-    throw new HTTPException(400, { "message": "Content-Type must be application/json" })
+    throw new HTTPException(400, {
+      message: 'Content-Type must be application/json',
+    });
   }
 
-  const body = await c.req.json()
+  const body = await c.req.json();
 
   if (!body.apiBaseURL && !body.tenant) {
-    throw new HTTPException(400, { "message": "apiBaseURL or tenant must be provided in the request body" })
+    throw new HTTPException(400, {
+      message: 'apiBaseURL or tenant must be provided in the request body',
+    });
   }
 
-  let apiURL
+  let apiURL;
 
   if (body.apiBaseURL) {
-    apiURL = new URL(body.apiBaseURL)
+    apiURL = new URL(body.apiBaseURL);
     if (!apiURL.hostname) {
-      throw new HTTPException(400, { "message": "apiBaseURL is not a valid URL" })
+      throw new HTTPException(400, {message: 'apiBaseURL is not a valid URL'});
     }
   } else {
-    apiURL = new URL(`https://${body.tenant}.api.identitynow.com`)
+    apiURL = new URL(`https://${body.tenant}.api.identitynow.com`);
     if (!apiURL.hostname) {
-      throw new HTTPException(400, { "message": "tenant is not valid" })
+      throw new HTTPException(400, {message: 'tenant is not valid'});
     }
-
   }
 
-  console.log(apiURL)
+  console.log(apiURL);
   if (!apiURL.origin) {
-    throw new HTTPException(400, { "message": "apiBaseURL or tenant provided is invalid" })
+    throw new HTTPException(400, {
+      message: 'apiBaseURL or tenant provided is invalid',
+    });
   }
 
-  const baseURL = apiURL?.origin
+  const baseURL = apiURL?.origin;
 
-  console.log("BaseAPIURL:", baseURL)
+  console.log('BaseAPIURL:', baseURL);
 
   try {
-    const authInfoResp = await fetch(baseURL + `/oauth/info`)
+    const authInfoResp = await fetch(baseURL + `/oauth/info`);
     if (!authInfoResp.ok) {
-      throw new Error("Error retrieving tenant info")
+      throw new Error('Error retrieving tenant info');
     }
-    const authInfo = await authInfoResp.json()
-    console.log(authInfo)
+    const authInfo = await authInfoResp.json();
+    console.log(authInfo);
 
     if (!authInfo || !authInfo.authorizeEndpoint) {
-      throw new Error("Error retrieving tenant info")
+      throw new Error('Error retrieving tenant info');
     }
 
-    const uuid = crypto.randomUUID()
-    const encryptionKey = generateRandomString(20)
-    
-    const objectToPut = { id: uuid, baseURL }
+    const uuid = crypto.randomUUID();
+    const encryptionKey = generateRandomString(20);
 
-    const state = {id: uuid, encryptionKey}
+    const objectToPut = {id: uuid, baseURL};
 
-    const authURL = new URL(authInfo.authorizeEndpoint) 
+    const state = {id: uuid, encryptionKey};
 
-    authURL.searchParams.set("client_id", clientId)
-    authURL.searchParams.set("response_type", "code")
-    authURL.searchParams.set("redirect_uri", body.dev === true ? devRedirectUrl : redirectUrl)
-    authURL.searchParams.set("state", btoa(JSON.stringify(state)))
+    const authURL = new URL(authInfo.authorizeEndpoint);
 
-    console.log("Generated Auth URL:", authURL.toString())
+    authURL.searchParams.set('client_id', clientId);
+    authURL.searchParams.set('response_type', 'code');
+    authURL.searchParams.set(
+      'redirect_uri',
+      body.dev === true ? devRedirectUrl : redirectUrl,
+    );
+    authURL.searchParams.set('state', btoa(JSON.stringify(state)));
 
-    const objectToRespond = { encryptionKey, authURL: authURL.toString(), ...objectToPut }
+    console.log('Generated Auth URL:', authURL.toString());
 
+    const objectToRespond = {
+      encryptionKey,
+      authURL: authURL.toString(),
+      ...objectToPut,
+    };
 
     try {
-      const data = await ddbDocClient.send(new PutCommand({ TableName: tableName, Item: objectToPut }));
-      console.log(data)
+      const data = await ddbDocClient.send(
+        new PutCommand({TableName: tableName, Item: objectToPut}),
+      );
+      console.log(data);
       if (!data) {
-        throw new HTTPException(400, { "message": "Error creating UUID" })
+        throw new HTTPException(400, {message: 'Error creating UUID'});
       }
-      return c.json(objectToRespond)
+      return c.json(objectToRespond);
     } catch (err) {
       //@ts-expect-error Unknown error shape
-      console.error("Error retrieving item:", err.message);
+      console.error('Error retrieving item:', err.message);
       //@ts-expect-error Unknown error shape
-      console.error("Error code:", err.code);
+      console.error('Error code:', err.code);
       //@ts-expect-error Unknown error shape
-      console.error("Error name:", err.name);
+      console.error('Error name:', err.name);
       //@ts-expect-error Unknown error shape
-      console.error("Error stack:", err.stack);
-  
-      throw new HTTPException(400, { "message": "Error creating UUID" })
+      console.error('Error stack:', err.stack);
+
+      throw new HTTPException(400, {message: 'Error creating UUID'});
     }
-
-
   } catch (err) {
-    throw new HTTPException(400, { "message": "Error retrieving tenant information" })
+    throw new HTTPException(400, {
+      message: 'Error retrieving tenant information',
+    });
   }
-})
+});
 
 // Exchange the code for a token, which is encrypted with the encryption key, and store the token in the database
 app.post('/Prod/sailapps/code/:code', async (c) => {
   const {code} = c.req.param();
   if (!code) {
-    throw new HTTPException(400, { "message": "Code not provided" })
+    throw new HTTPException(400, {message: 'Code not provided'});
   }
 
-  const { state } = c.req.query()
+  const {state} = c.req.query();
 
   if (!state) {
-    throw new HTTPException(400, { "message": "State not provided" })
+    throw new HTTPException(400, {message: 'State not provided'});
   }
 
-  const { id: uuid, encryptionKey } = JSON.parse(atob(state))
+  const {id: uuid, encryptionKey} = JSON.parse(atob(state));
 
-  console.log(`Parsed State UUID: ${uuid}, Encryption Key: ${encryptionKey}`)
+  console.log(`Parsed State UUID: ${uuid}, Encryption Key: ${encryptionKey}`);
+
+  let tableData;
 
   try {
-    const data = await ddbDocClient.send(new GetCommand({ TableName: tableName, Key: { id: uuid } }));
+    const data = await ddbDocClient.send(
+      new GetCommand({TableName: tableName, Key: {id: uuid}}),
+    );
     if (!data.Item) {
-      throw new HTTPException(400, { "message": "Error retrieving table data" })
+      throw new HTTPException(400, {message: 'Error retrieving table data'});
     }
-
-    console.log(data.Item)
-    
-    console.log("Exchanging code for token")
-
-    if (!data.Item.baseURL) {
-      throw new HTTPException(400, { "message": "baseURL not populated" })
-    }
-    
-    const tokenExchangeURL = new URL(data.Item.baseURL + `/oauth/token`)
-    tokenExchangeURL.searchParams.set("grant_type", "authorization_code")
-    tokenExchangeURL.searchParams.set("client_id", clientId)
-    tokenExchangeURL.searchParams.set("code", code)
-    tokenExchangeURL.searchParams.set("redirect_uri", redirectUrl)
-
-    const tokenExchangeResp = await fetch(tokenExchangeURL, {
-      method: "POST",
-    })
-
-    if (!tokenExchangeResp.ok) {
-      throw new HTTPException(400, { "message": "Error exchanging code for token" })
-    }
-
-    const tokenExchangeData = await tokenExchangeResp.json()
-
-    console.log(tokenExchangeData)
-
-    if (!tokenExchangeData.access_token) {
-      throw new HTTPException(400, { "message": "Error exchanging code for token" })
-    }
-    
-    const iv = crypto.randomBytes(16);
-    const cipher = createCipheriv('aes-256-cbc', encryptionKey, iv);
-    
-    let encryptedToken = cipher.update(JSON.stringify(tokenExchangeData), 'utf8', 'hex');
-    encryptedToken += cipher.final('hex');
-
-    const encryptedTokenWithIv = iv.toString('hex') + ':' + encryptedToken;
-
-    try {
-      const data = await ddbDocClient.send(new UpdateCommand({
-        TableName: tableName, Key: { id: uuid }, UpdateExpression: "set token = :token",
-        ExpressionAttributeValues: {
-          ":token": encryptedTokenWithIv,
-        }
-      }));
-      console.log(data)
-      if (!data) {
-        throw new HTTPException(400, { "message": "Error adding token" })
-      }
-      return c.json({message: "Token added successfully"}, 200)
-    } catch (err) {
-      //@ts-expect-error Unknown error shape
-      console.error("Error retrieving item:", err.message);
-      //@ts-expect-error Unknown error shape
-      console.error("Error code:", err.code);
-      //@ts-expect-error Unknown error shape
-      console.error("Error name:", err.name);
-      //@ts-expect-error Unknown error shape
-      console.error("Error stack:", err.stack);
-  
-      throw new HTTPException(400, { "message": "Error creating UUID" })
-    }
-
+    tableData = data.Item;
   } catch (err) {
     //@ts-expect-error Unknown error shape
-    console.error("Error retrieving item:", err.message);
+    console.error('Error retrieving item:', err.message);
     //@ts-expect-error Unknown error shape
-    console.error("Error code:", err.code);
+    console.error('Error code:', err.code);
     //@ts-expect-error Unknown error shape
-    console.error("Error name:", err.name);
+    console.error('Error name:', err.name);
     //@ts-expect-error Unknown error shape
-    console.error("Error stack:", err.stack);
+    console.error('Error stack:', err.stack);
 
-    throw new HTTPException(400, { "message": "Error retrieving table data" })
+    throw new HTTPException(400, {message: 'Error retrieving table data'});
   }
-})
+
+  console.log(tableData);
+
+  console.log('Exchanging code for token');
+
+  if (!tableData.baseURL) {
+    throw new HTTPException(400, {message: 'baseURL not populated'});
+  }
+
+  const tokenExchangeURL = new URL(tableData.baseURL + `/oauth/token`);
+  tokenExchangeURL.searchParams.set('grant_type', 'authorization_code');
+  tokenExchangeURL.searchParams.set('client_id', clientId);
+  tokenExchangeURL.searchParams.set('code', code);
+  tokenExchangeURL.searchParams.set('redirect_uri', redirectUrl);
+
+  const tokenExchangeResp = await fetch(tokenExchangeURL, {
+    method: 'POST',
+  });
+
+  if (!tokenExchangeResp.ok) {
+    throw new HTTPException(400, {message: 'Error exchanging code for token'});
+  }
+
+  const tokenExchangeData = await tokenExchangeResp.json();
+
+  console.log(tokenExchangeData);
+
+  if (!tokenExchangeData.access_token) {
+    console.error('Token Exchange Response:', JSON.stringify(tokenExchangeData, null, 2));
+    throw new HTTPException(400, {message: 'Error exchanging code for token'});
+  }
+
+  const iv = crypto.randomBytes(16);
+  const cipher = createCipheriv('aes-256-cbc', encryptionKey, iv);
+
+  let encryptedToken = cipher.update(
+    JSON.stringify(tokenExchangeData),
+    'utf8',
+    'hex',
+  );
+  encryptedToken += cipher.final('hex');
+
+  const encryptedTokenWithIv = iv.toString('hex') + ':' + encryptedToken;
+
+  try {
+    const data = await ddbDocClient.send(
+      new UpdateCommand({
+        TableName: tableName,
+        Key: {id: uuid},
+        UpdateExpression: 'set token = :token',
+        ExpressionAttributeValues: {
+          ':token': encryptedTokenWithIv,
+        },
+      }),
+    );
+    console.log(data);
+    if (!data) {
+      throw new HTTPException(400, {message: 'Error adding token'});
+    }
+    return c.json({message: 'Token added successfully'}, 200);
+  } catch (err) {
+    //@ts-expect-error Unknown error shape
+    console.error('Error retrieving item:', err.message);
+    //@ts-expect-error Unknown error shape
+    console.error('Error code:', err.code);
+    //@ts-expect-error Unknown error shape
+    console.error('Error name:', err.name);
+    //@ts-expect-error Unknown error shape
+    console.error('Error stack:', err.stack);
+    console.error('Full Error:', JSON.stringify(err, null, 2));
+
+    throw new HTTPException(400, {message: 'Error adding token'});
+  }
+});
 
 app.get('/Prod/sailapps/uuid/:uuid', async (c) => {
   const uuid = c.req.param('uuid');
   if (!uuid) {
-    throw new HTTPException(400, { "message": "UUID not provided" })
+    throw new HTTPException(400, {message: 'UUID not provided'});
   }
   try {
-    const data = await ddbDocClient.send(new GetCommand({ TableName: tableName, Key: { id: uuid } }));
-    console.log(data?.Item)
+    const data = await ddbDocClient.send(
+      new GetCommand({TableName: tableName, Key: {id: uuid}}),
+    );
+    console.log(data?.Item);
     if (!data?.Item?.token) {
-      throw new HTTPException(400, { "message": "Token not populated" })
+      throw new HTTPException(400, {message: 'Token not populated'});
     }
 
-    return c.json(data.Item, 200)
-
+    return c.json(data.Item, 200);
   } catch (err) {
     //@ts-expect-error Unknown error shape
-    console.error("Error retrieving item:", err.message);
+    console.error('Error retrieving item:', err.message);
     //@ts-expect-error Unknown error shape
-    console.error("Error code:", err.code);
+    console.error('Error code:', err.code);
     //@ts-expect-error Unknown error shape
-    console.error("Error name:", err.name);
+    console.error('Error name:', err.name);
     //@ts-expect-error Unknown error shape
-    console.error("Error stack:", err.stack);
+    console.error('Error stack:', err.stack);
 
-    throw new HTTPException(400, { "message": "Error retrieving token" })
+    throw new HTTPException(400, {message: 'Error retrieving token'});
   }
-})
+});
 
-export const handler = handle(app)
+export const handler = handle(app);

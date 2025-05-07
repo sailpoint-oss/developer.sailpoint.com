6b6c19bdbda83aa3d8256d089edd4831ce4d17be
Luke Hagar <lukeslakemail@gmail.com>
2025-05-07 11:47:52 -0500

    Fix the page

--- src/services/SailAppsService.ts
+++ src/services/SailAppsService.ts
@@ -1,6 +1,7 @@
 export async function sendCode(gatewayUrl: string, code: string, state: string) {
-  const url = new URL(`${gatewayUrl}sailapps/code/${code}`);
+  const url = new URL(`${gatewayUrl}sailapps/code`);
   url.searchParams.set('state', state);
+  url.searchParams.set('code', code);
   try {
     const response = await fetch(url, {
       method: 'POST',

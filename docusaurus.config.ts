6269db94edb5e9b1f8e30ae9450bbe21104038ae
Tyler Mairose <tyler.mairose@sailpoint.com>
2026-09-25 22:57:24 -0400

    Add llms plugin to developer site

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -166,7 +166,11 @@ const config: Config = {
     }
   },
 
-  themes: ['docusaurus-theme-openapi-docs', '@docusaurus/theme-mermaid'],
+  themes: [
+    'docusaurus-theme-openapi-docs',
+    '@docusaurus/theme-mermaid',
+    '@writechoice/docusaurus-theme-llms-txt',
+  ],
 
   customFields: {
     AUTH_USERNAME: process.env.AUTH_USERNAME || '',
